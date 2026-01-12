725d6b2a0c18911dde44977a7dd9e03b86214677
appenteng <frimsco@yahoo.com>
2026-01-11 21:02:33 -0800

    created zip_creator in bonus 16

--- bonus/BONUS16.py
+++ bonus/BONUS16.py
@@ -1,18 +1,32 @@
 import PySimpleGUI as sg
+from Scripts.rst2odt import output
+
+from zip_creator import make_archive
 
 label1 = sg.Text("select files to compress")
 input1 = sg.Input()
-button1 = sg.FilesBrowse("Click Me")
+button1 = sg.FilesBrowse("Choose", key="files")
 
 label2 = sg.Text("select destination folder")
 input2 = sg.Input()
-button2 = sg.FolderBrowse("Click Me")
+button2 = sg.FolderBrowse("choose", key="folder")
+
+compress_button = sg.Button("Compress Files")
+output_label= sg.Text(key=output, size=(40,1), text_color="green")
 
 windows = sg.Window("File Compressor",
                     layout=[[label1, input1, button1],
                             [label2, input2, button2],
-                            [sg.Button("Compress Files")]
-])
+                            [compress_button, output_label]])
+while True:
+    event, values =windows.read()
+    print(event, values)
+    filepaths = values["files"].split(';')
+    folder = values["folder"]
+    make_archive(filepaths, folder)
+    windows["output"].update("Files compressed successfully!")
+    if event == sg.WIN_CLOSED:
+        break
 
-windows.read()
+
 windows.close()
